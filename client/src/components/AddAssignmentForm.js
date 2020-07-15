b37aec6bbc564419e48d71c996984f37ba650060
lvarnum <lvarnum@email.arizona.edu>
2020-07-14 17:57:45 -0700

    Created timelines for schedule and assignments

--- client/src/components/AddAssignmentForm.js
+++ client/src/components/AddAssignmentForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Button, TextField, Container, Grid, Paper, Typography, Dialog, DialogActions, DialogContent } from '@material-ui/core';
+import { IconButton, Button, TextField, Grid, Paper, Typography, Dialog, DialogActions, DialogContent } from '@material-ui/core';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
 
 function AddAssignmentForm(props) {
     const { formObject, handleFormSubmit, handleInputChange } = props;
@@ -17,65 +18,64 @@ function AddAssignmentForm(props) {
     return (
         <>
             <Paper>
-                <Container maxWidth="sm">
-                    <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-                        Add Assignment
-                    </Button>
-                    <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-                        <DialogContent>
-                            <form>
-                                <Grid container spacing={3}>
-                                    <Grid item xs={12}>
-                                        <Typography variant="h4" gutterBottom>
-                                            Add Assignment
-                                </Typography>
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <TextField
-                                            label="Assignment Name"
-                                            value={formObject.name}
-                                            name="name"
-                                            onChange={handleInputChange}
-                                            as="input"
-                                            type="text"
-                                        />
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <TextField
-                                            label="Assignment Notes"
-                                            value={formObject.notes}
-                                            name="notes"
-                                            onChange={handleInputChange}
-                                            as="input"
-                                            type="text"
-                                        />
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        <TextField
-                                            id="date"
-                                            label="Due Date"
-                                            name="due"
-                                            value={formObject.due}
-                                            type="date"
-                                            onChange={handleInputChange}
-                                            InputLabelProps={{
-                                                shrink: true,
-                                            }}
-                                        />
-                                    </Grid>
-                                    <Button variant="contained" color="secondary" type="submit" onClick={handleFormSubmit}>
+                <IconButton aria-label="add" size="medium" color="primary" onClick={handleClickOpen}>
+                    <AddCircleIcon size="large" color="secondary" style={{ marginRight: "5px" }} />
+                Add Assignment
+                </IconButton>
+                <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                    <DialogContent>
+                        <form>
+                            <Grid container spacing={3}>
+                                <Grid item xs={12}>
+                                    <Typography variant="h4" gutterBottom>
                                         Add Assignment
-                                </Button>
+                                </Typography>
                                 </Grid>
-                            </form>
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={handleClose} color="primary">
-                                Close
+                                <Grid item xs={12}>
+                                    <TextField
+                                        label="Assignment Name"
+                                        value={formObject.name}
+                                        name="name"
+                                        onChange={handleInputChange}
+                                        as="input"
+                                        type="text"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        label="Assignment Notes"
+                                        value={formObject.notes}
+                                        name="notes"
+                                        onChange={handleInputChange}
+                                        as="input"
+                                        type="text"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        id="date"
+                                        label="Due Date"
+                                        name="due"
+                                        value={formObject.due}
+                                        type="date"
+                                        onChange={handleInputChange}
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                    />
+                                </Grid>
+                                <Button variant="contained" color="secondary" type="submit" onClick={handleFormSubmit}>
+                                    Add Assignment
+                                </Button>
+                            </Grid>
+                        </form>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose} color="primary">
+                            Close
                         </Button>
-                        </DialogActions>
-                    </Dialog>
-                </Container>
+                    </DialogActions>
+                </Dialog>
             </Paper>
         </>
     )
