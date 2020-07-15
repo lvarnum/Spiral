aab9cc82677dba9517d68cac1b49f8f45b213c8d
lvarnum <lvarnum@email.arizona.edu>
2020-07-14 21:33:22 -0700

    Worked on Assignment Timeline Items

--- client/src/pages/Assignments.js
+++ client/src/pages/Assignments.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { AddAssignmentForm, AssignmentTimeline } from "../components";
-import { Grid, Typography, Paper, Divider } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import { useLocation } from 'react-router-dom'
 import API from "../utils/API";
 
@@ -17,21 +17,18 @@ function Assignments(props) {
     const location = useLocation();
 
     useEffect(() => {
+        loadAssignments();
+    }, [props.user.id]);
+
+    const loadAssignments = () => {
         API.ScheduleItem.getById(location.course)
             .then(res => {
                 setCourse({ course: res.data.course })
                 var assignments = [];
                 res.data.assignments.forEach(item => assignments.push(item));
-                for (var i = 0; i < assignments.length - 1; i++) {
-                    if (assignments[i].due > assignments[i + 1].due) {
-                        var temp = assignments[i];
-                        assignments[i] = assignments[i + 1];
-                        assignments[i + 1] = temp;
-                    }
-                }
                 setAssignments({ assignments: assignments });
             });
-    }, [props.user.id]);
+    }
 
     const handleInputChange = (event) => {
         event.preventDefault();
@@ -55,8 +52,10 @@ function Assignments(props) {
                     {
                         $push: { assignments: res.data._id }
                     });
+            }).then(res => {
+                setFormObject(initialFormState);
+                loadAssignments();
             });
-        setFormObject(initialFormState);
     }
 
     return (
@@ -68,7 +67,6 @@ function Assignments(props) {
                 <Grid item xs={5}>
                     <Typography variant="h5">{location.session} Session</Typography>
                 </Grid>
-                <Divider variant="middle" />
                 <Grid item xs={5}>
                     <AddAssignmentForm
                         formObject={formObject}
@@ -78,21 +76,6 @@ function Assignments(props) {
                 </Grid>
             </Grid>
             <Grid container spacing={0} justify="center" direction="row" align="center">
-                <Grid item xs={3}>
-                    <Paper style={{ backgroundColor: '#2c387e', padding: "10px" }}>
-                        <Typography variant="h2" style={{ color: "white" }}>A</Typography>
-                        <Typography variant="h2" style={{ color: "white" }}>S</Typography>
-                        <Typography variant="h2" style={{ color: "white" }}>S</Typography>
-                        <Typography variant="h2" style={{ color: "white" }}>I</Typography>
-                        <Typography variant="h2" style={{ color: "white" }}>G</Typography>
-                        <Typography variant="h2" style={{ color: "white" }}>N</Typography>
-                        <Typography variant="h2" style={{ color: "white" }}>M</Typography>
-                        <Typography variant="h2" style={{ color: "white" }}>E</Typography>
-                        <Typography variant="h2" style={{ color: "white" }}>N</Typography>
-                        <Typography variant="h2" style={{ color: "white" }}>T</Typography>
-                        <Typography variant="h2" style={{ color: "white" }}>S</Typography>
-                    </Paper>
-                </Grid >
                 <Grid item xs={9}>
                     <AssignmentTimeline
                         assignmentState={assignmentState}
