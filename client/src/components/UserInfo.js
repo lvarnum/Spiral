ad73bdd1ae2a1550eabdd10d0295869c1a7f6132
JRPino <pino3789@gmail.com>
2020-07-16 23:11:51 -0700

    Added in api skeleton

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -1,41 +1,56 @@
-import React, { useState, useEffect  }from 'react'
-import { Button, TextField, Container, Grid, Paper, Typography, Dialog, DialogActions, DialogContent } from '@material-ui/core';
+import React, { useState, useEffect } from 'react'
+import { Grid, Typography } from '@material-ui/core';
 import API from "../utils/API";
 
 
 
 
 function UserInfo(props) {
-    const { formObject } = props;
+    const [scheduleState, setSchedule] = useState({
+        schedule: []
+    });
 
-   useEffect(() => {
-            API.User.getById(props.user.id)
-            console.log(props.user.id)
-                .then(res => {
-                    // setSession({ session: res.data.session })
-                    // var courses = [];
-                    // res.data.scheduleItems.forEach(item => {
-                    //     API.ScheduleItem.getById(item)
-                    //         .then(res => courses.push(res.data));
-                    // });
-                    // API.University.getById(res.data.university)
-                    //     .then(res => {
-                    //         setUniversity({ university: res.data });
-                    //         setSchedule({ schedule: courses })
-                    //     });
-                });
-        }, [props.user.id])
+    const [universityState, setUniversity] = useState({
+        university: ""
+    });
+    useEffect(() => {
+        loadUserInfo();
+    }, [props.user.id]);
 
-   
-    return (
+    const loadUserInfo = () => {
+        API.User.getById(props.user.id)
+        .then(res => {
+            setSchedule({ schedule: res.data.scheduleItems });
+        });
+    
+    API.University.getById(props.user.university)
+        .then(res => setUniversity({ university: res.data }))
 
-       
+    };
 
-       <div>
-             <h1>User Info</h1>
-             <h1>users name: {formObject.firstName} </h1>
-        </div>
-    )
 
- };
+return (
+    <>
+        <Grid container spacing={2} direction="column" align="center" justify="center" alignItems="center"
+            style={{ border: "solid 2px #2c387e", marginBottom: "15px" }}>
+            <Grid item xs={12}>
+                <Typography variant="h2">{props.user.firstName}</Typography>
+            </Grid >
+            <Grid item xs={12}>
+                <Typography variant="h2">{props.user.email}</Typography>
+            </Grid >
+            <Grid item xs={12}>
+                <Typography variant="h4">{universityState.university.name}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="h5">{props.user.session} Session</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Typography variant="h5">{props.user.classes}</Typography>
+            </Grid>
+        </Grid>
+    </>
+)
+};
+
 export default UserInfo
